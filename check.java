641ac05ed9a0299a4ddbe2449058368ed0748156
ArnavvCodes <arnavhaisingh@gmail.com>
2025-10-05 23:22:16 +0530

    INFINTE LOOP (FOR LOOP)

---
+++ check.java
@@ -0,0 +1,9 @@
+
+public class check {
+    public static void main(String arg[]){
+        //Scanner sc = new Scanner(System.in);
+        for (int i=1; ; ) {
+            System.out.println("Arnav Singh");
+        }
+    }
+}
